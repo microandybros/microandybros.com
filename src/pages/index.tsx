ab8d4e9e3195365ce90a2d2bddc5358e11a844d1
Andrew <andrew.meyer@hornbach.com>
2019-09-03 11:56:35 +0200

    Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+const Header = styled.h1`
+  background-color: #f0f;
+  flex: 1;
+  text-align: center;
+`
+
+export default () => <Header>Hello world!</Header>
