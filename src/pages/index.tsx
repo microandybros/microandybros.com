f8c66f2cc3d67ac85f8cea61b96d096617152e57
Andreas Jacob <cordobo@web.de>
2019-09-04 08:47:30 +0200

    Fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,21 @@ import "../styles/global.css"
 
 export default () => {
   return (
-    <h2 className="hello">Hello world!</h2>
+    <>
+      <div className="container">
+        <header>
+          <h1 id="logo">13bit Micro Andy Bros</h1>
+        </header>
+        <main>
+          <div className="content">
+            <h1 className="headline">Micro Andy Bros</h1>
+            <h2 className="subHeadline">Inside joke meets business</h2>
+          </div>
+          <div className="sidebar">
+            Sidebar
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
